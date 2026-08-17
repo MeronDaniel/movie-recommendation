45ac6d72959ff1dbd36fa8e03bafaed7f12cac23
meron_daniel <dmeron29@gmail.com>
2026-08-17 14:33:23 -0400

    when user clicks on poster the plot summary displays beneath

--- frontend/src/components/Display.vue
+++ frontend/src/components/Display.vue
@@ -2,8 +2,16 @@
   <div class="image-wrapper">
     <div class="image-card">
       <h1>{{ movie_title }}</h1>
-      <img :src="poster_url" alt="Movie Poster" />
-      
+
+      <img :src="poster_url" 
+      @click="handleClick"
+      alt="Movie Poster" />
+
+      <div v-if="showPlot"> <!--Display the plot summary when the poster is clicked-->
+        <h2>Plot Summary</h2>
+        <p>{{ movie_plot }}</p>
+      </div> 
+
     </div>
   </div>
 </template>
@@ -14,7 +22,15 @@ export default {
   data() {
     return {
       poster_url: '',
-      movie_title: ''
+      movie_title: '',
+      movie_plot: '',
+      showPlot: false
+    }
+  },
+  methods: {
+    handleClick() {
+      this.showPlot = !this.showPlot
+      // Display the plot summary when the poster is clicked
     }
   },
   mounted() {
@@ -28,9 +44,9 @@ export default {
       
       this.poster_url = parsedMovie.movie.Poster
       this.movie_title = parsedMovie.movie.Title
+      this.movie_plot = parsedMovie.movie.Plot
 
-    }
-    
+    } 
   }
 }
 </script>
@@ -44,11 +60,12 @@ export default {
 }
   
 .image-card {
-  background-color: #fff;
+  background-color: #4389ec;
   padding: 2rem;
   align-items: center;
   border-radius: 8px;
   box-shadow: 0 4px 6px rgba(112, 101, 101, 0.1);
+  color: #000;
   width: 350px;
 }
 </style>
